3d437bad31eae0951d13f4f635cb89d64f67b512
Chrotesque <github@chrotesque.com>
2023-10-05 09:31:30 +0000

    Removing pagination, datetime format change

--- cip5_api/settings.py
+++ cip5_api/settings.py
@@ -33,10 +33,7 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DATETIME_FORMAT': '%d %b %Y',
+    'DATETIME_FORMAT': 'iso-8601',
 }
 
 if 'DEV' not in os.environ:
