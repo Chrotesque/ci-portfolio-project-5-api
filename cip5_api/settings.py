8dd0e062bfc950935b4150bc1f38a849f4df72ad
Chrotesque <github@chrotesque.com>
2023-10-05 14:01:03 +0000

    Change to pagination, related to #2

--- cip5_api/settings.py
+++ cip5_api/settings.py
@@ -34,8 +34,7 @@ REST_FRAMEWORK = {
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
+        None,
     'DATETIME_FORMAT': '%d %b %Y %H:%M',
 }
 
